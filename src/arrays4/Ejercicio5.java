081dd9791338e55a96096b2f19cbef77f63d4f2d
Elena <Elena@106-01.info2>
2023-01-30 09:17:37 +0100

    Eliminación - Ejercicio 5

--- src/arrays4/Ejercicio5.java
+++ src/arrays4/Ejercicio5.java
@@ -1,9 +1,53 @@
 package arrays4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
+		// Necesitaremos una tabla para almacenar los números favoritos
+		int tabla[];
 		
+		// Cantidad de números favoritos que tiene
+		int numFav;
+		
+		// Variable donde vamos guardando los números favoritos
+		int numero;
+		
+		// Indices aleatorios
+		int num1, num2;
+		
+		// Creación del Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pedimos al usuario cuántos números favoritos tiene
+		System.out.println("¿Cuántos números favoritos tiene?");
+		numFav = sc.nextInt();
+		
+		// Creamos la tabla de tamaño numFav
+		tabla = new int[numFav];
+		
+		// Le pedimos al usuario sus números favoritos
+		// y lo almacenamos en la tabla
+		for(int i=0; i<tabla.length; i++) {
+			System.out.println("Introduzca uno de sus números favoritos:");
+			numero = sc.nextInt();
+			tabla[i] = numero;
+		}
+		
+		// Ordenamos la tabla
+		Arrays.sort(tabla);
+		System.out.println(Arrays.toString(tabla));
+		
+		num1 = (int) (Math.random()*tabla.length);
+		do {
+			num2 = (int) (Math.random()*tabla.length);
+		} while (num1 == num2);
+		
+		System.out.println(num1 + " " + num2);
+		
+		sc.close();
 	}
 
 }
