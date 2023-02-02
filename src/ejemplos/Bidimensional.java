e586912e261a27cdd94329e23c41f9d7dab699bd
Elena <Elena@106-01.info2>
2023-02-02 13:27:13 +0100

    Ejemplos de funciones y bidimensionales. Bidimensionales - Ejercicio 5 corregido.

--- src/ejemplos/Bidimensional.java
+++ src/ejemplos/Bidimensional.java
@@ -5,37 +5,19 @@ import java.util.Arrays;
 public class Bidimensional {
 
 	public static void main(String[] args) {
-		int uni[] = { 1, 3, 6, 8 };
 
-		int tabla[][] = { { 1, 3, 6, 8, 5, 1 }, { 1, 3, 6, 8, 4, 7 }, { 1, 3, 6, 8, 9, 5 }, { 1, 3, 6, 8, 1, 5 } };
-
-		// Imprimir tabla de dos dimensiones
-		System.out.println(Arrays.deepToString(tabla));
+		int tabla[][] = 
+				{ { 1, 3, 6, 8, 5, 1 }, 
+				{ 1, 3, 6, 8, 4, 7 }, 
+				{ 1, 3, 6, 8, 9, 5 }, 
+				{ 1, 3, 6, 8, 1, 5 } };
 		
-		for(int i=0; i<tabla.length; i++) {
-			for (int j=0; j<tabla[i].length; j++) {
-				System.out.print(tabla[i][j] + " ");
+		for(int col = 0; col<tabla[0].length; col++) {
+			for(int fila =0; fila<tabla.length; fila++) {
+				System.out.print(tabla[fila][col] + " ");
 			}
 			System.out.println();
-		}
-		
-//		tabla[0][1] = 24;
-//		
-//		for (int i = 0; i < 4; i++) {
-//			for (int j = 0; j < 6; j++) {
-//				System.out.print(tabla[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
-//
-//		System.out.println(Arrays.deepToString(tabla));
-//
-//		for (int i = 0; i < 4; i++) {
-//			System.out.println(Arrays.toString(tabla[i]));
-//		}
-//
-//		System.out.println("Filas: " + tabla.length);
-//		System.out.println("Columnas: " + tabla[1].length);
+		}	
 	}
 
 }
