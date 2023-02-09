186d28e50199c1deee5d564fd3be6a409fff8195
Elena <Elena@106-01.info2>
2023-02-09 12:43:31 +0100

    Pruebas de funciones

--- src/funciones/Principal.java
+++ src/funciones/Principal.java
@@ -1,11 +1,14 @@
 package funciones;
 
+import java.util.Arrays;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		int[] tabla = { 2, 3, 6, 5, 2, 5, 8, 7, 34 };
-		long suma = Ejercicio1.sumaTabla(tabla);
-		System.out.println("La suma de los elementos de la tabla es: " + suma);
+		int[] result = Ejercicio5.sinRepetidos(tabla);
+		System.out.println(Arrays.toString(tabla));
+		System.out.println(Arrays.toString(result));
 	}
 
 }
